ef8c15928de5e4e5adea9421d015b1c25a1fd7af
iknow <iknowxw@gmail.com>
2015-01-21 18:28:25 +0800

    完成小步语义

--- simple.rb
+++ simple.rb
@@ -1,4 +1,4 @@
-# simple的实现
+# 小步语义的实现
 # create by xiongwei
 
 # 表达式
@@ -125,17 +125,132 @@ class LessThen < Struct.new(:left, :right)
 	end
 end
 
+# 语句
+#  空语句
+class DoNothing
+	def to_s
+		'do_nothing'
+	end
+	
+	def inspect
+		"<<#{self}>>"
+	end
+	
+	def ==(other_statmement)
+		other_statmement.instance_of?( DoNothing)
+	end
+	
+	def reducible?
+		false
+	end
+end
+
+#  赋值
+class Assign < Struct.new(:name, :expression)
+	def to_s
+		"#{name} = #{expression}"
+	end
+	
+	def inspect
+		"<<#{self}>>"
+	end
+	
+	def reducible?
+		true
+	end
+	
+	def reduce( environment)
+		if expression.reducible?
+			[Assign.new( name, expression.reduce( environment)), environment]
+		else
+			[DoNothing.new, environment.merge( {name => expression})]
+		end
+	end
+end
+
+#  If语句
+class If < Struct.new( :condition, :consequence, :alternative)
+	def to_s
+		"if (#{condition}) { #{consequence} } else { #{alternative} }"
+	end
+	
+	def inspect
+		"<<#{self}>>"
+	end
+	
+	def reducible?
+		true
+	end
+	
+	def reduce( environment)
+		if condition.reducible?
+			[If.new( condition.reduce( environment), consequence, alternative), environment]
+		else
+			case condition
+			when Boolean.new( true)
+				[consequence, environment]
+			when Boolean.new( false)
+				[alternative, environment]
+			end
+		end
+	end
+end
+
+#  Sequence序列
+class Sequence < Struct.new(:first, :second)
+	def to_s
+		"#{first}; #{second}"
+	end
+	
+	def inspect
+		"#{self}"
+	end
+	
+	def reducible?
+		true
+	end
+	
+	def reduce( environment)
+		case first
+		when DoNothing.new
+			[second, environment]
+		else
+			reduced_first, reduced_environment = first.reduce( environment)
+			[Sequence.new( reduced_first, second), reduced_environment]
+		end
+	end
+end
+
+#  While循环
+class While < Struct.new(:condition, :body)
+	def to_s
+		"while (#{condition}) { #{body} }"
+	end
+	
+	def inspect
+		"<<#{self}>>"
+	end
+	
+	def reducible?
+		true
+	end
+	
+	def reduce( environment)
+		[If.new( condition, Sequence.new( body, self), DoNothing.new), environment]
+	end
+end
+
 # 虚拟机
-class Machine < Struct.new(:expression, :environment)
+class Machine < Struct.new(:statement, :environment)
 	def step
-		self.expression = expression.reduce( environment)
+		self.statement, self.environment = statement.reduce( environment)
 	end
 	
 	def run
-		while expression.reducible?
-			puts expression
+		while statement.reducible?
+			puts "#{statement}, #{environment}"
 			step
 		end
-		puts expression
+		puts "#{statement}, #{environment}"
 	end
 end
---
+++ simple_test.rb
@@ -0,0 +1,71 @@
+# 小步语义测试代码
+# Create by xiongwei
+
+# 虚拟机运算
+Machine.new(
+Add.new(
+Multiply.new( Number.new(1), Number.new(2)),
+Multiply.new( Number.new(3), Number.new(4))
+)
+).run
+
+# 小于运算
+Machine.new(
+LessThen.new( Number.new(5), Add.new( Number.new(2), Number.new(2)))
+).run
+
+# 加入变量
+Machine.new(
+Add.new( Variable.new(:x), Variable.new(:y)),
+{ x: Number.new(3), y: Number.new(4) }
+).run
+
+# 赋值表达式
+statement = Assign.new(:x, Add.new( Variable.new(:x), Number.new(1)))
+environment = { x: Number.new(2) }
+statement.reducible?
+statement, environment = statement.reduce( environment)
+statement, environment = statement.reduce( environment)
+statement, environment = statement.reduce( environment)
+statement.reducible?
+#  虚拟机版
+Machine.new(
+Assign.new( :x, Add.new( Variable.new(:x), Variable.new(:y))),
+{ x: Number.new(3), y: Number.new(4) }
+).run
+
+# If语句
+Machine.new(
+If.new(
+Variable.new(:x),
+Assign.new(:y, Number.new(1)),
+Assign.new(:y, Number.new(2))
+),
+{ x: Boolean.new(true) }
+).run
+Machine.new(
+If.new(
+Variable.new(:x),
+Assign.new(:y, Number.new(1)),
+DoNothing.new
+),
+{ x: Boolean.new(false) }
+).run
+
+# Sequence序列
+Machine.new(
+Sequence.new(
+Assign.new(:x, Add.new( Number.new(1), Number.new(1))),
+Assign.new(:y, Add.new( Variable.new(:x), Number.new(3)))
+),
+{}
+).run
+
+# While循环
+Machine.new(
+While.new(
+LessThen.new(Variable.new(:x), Number.new(5)),
+Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+),
+{ x: Number.new(1) }
+).run
