5554021a2181d08a1f6243b4eb01d9d12c5d4376
iknow0614 <xiongwei_13@hotmail.com>
2015-02-10 16:39:01 +0800

    NFA和DFA的等价性，从NFA转化为DFA

--- pattern_test.rb
+++ pattern_test.rb
@@ -133,3 +133,39 @@ parse_tree = PatternParser.new.parse( '(a(|b))*')
 pattern = parse_tree.to_ast
 pattern.matches?( 'abaab')
 pattern.matches?( 'abba')
+
+# 等价性 NFA转DFA
+rulebook = NFARulebook.new( [
+FARule.new( 1, 'a', 1), FARule.new( 1, 'a', 2), FARule.new( 1, nil, 2),
+FARule.new( 2, 'b', 3),
+FARule.new( 3, 'b', 1), FARule.new( 3, nil, 2)
+])
+nfa_design = NFADesign.new( 1, [3], rulebook)
+nfa_design.to_nfa.current_states
+nfa_design.to_nfa( Set[ 2]).current_states
+nfa_design.to_nfa( Set[ 3]).current_states
+
+nfa = nfa_design.to_nfa( Set[ 2, 3])
+nfa.read_character( 'b'); nfa.current_states
+
+simulation = NFASimulation.new( nfa_design)
+simulation.next_state( Set[ 1, 2], 'a')
+simulation.next_state( Set[ 1, 2], 'b')
+simulation.next_state( Set[ 3, 2], 'b')
+simulation.next_state( Set[ 1, 3, 2], 'b')
+simulation.next_state( Set[ 1, 3, 2], 'a')
+
+rulebook.alphabet
+simulation.rules_for( Set[ 1, 2])
+simulation.rules_for( Set[ 3, 2])
+
+start_state = nfa_design.to_nfa.current_states
+simulation.discover_states_and_rules( Set[ start_state])
+
+nfa_design.to_nfa( Set[ 1, 2]).accepting?
+nfa_design.to_nfa( Set[ 2, 3]).accepting?
+
+dfa_design = simulation.to_dfa_design
+dfa_design.accepts?( 'aaa')
+dfa_design.accepts?( 'aab')
+dfa_design.accepts?( 'bbbabb')
